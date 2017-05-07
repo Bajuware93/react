b36921c29f69239ba38c8d1a72207ad73dc098e6
Unknown <timo.engler@web.de>
2017-05-07 09:09:55 +0200

    Projetkordner aufgeräumt

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,3 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class Component extends React.Component{
+constructor(){
+super();
+this.state = {
+value: 'Anfangsstatus'
+};
+}
+render(){
+return(
+<div>
+<button className="component" onClick={() =>
+this.setState({value: 'geklickt'})}>{this.state.value}</button>
+</div>
+);
+}
+}
+
+class Props extends React.Component{
+render(){
+return(
+<div>Ich esse gerne: {this.props.essen}</div>
+);
+}
+}
+
+/*class MainComponent extends React.Component{
+/* Die Funktion gibt Component aus und fügt die props value hinzu
+Auf value kann mit {this.props.value} in Component zugegriffen
+werden
+
+render_button(){
+return <Component value='Value aus Main' />
+}
+
+render(){
+return (
+this.render_button()
+)
+
+}
+
+} */
+
+ReactDOM.render(
+<div>
+<Component />
+<Props essen="Döner" />
+<Props essen="Pizza" />
+</div>,
+document.getElementById('root')
+);
+
+
+
